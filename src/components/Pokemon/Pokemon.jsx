4cd26747c2b0723e2c7db41f6674199bf68cc4df
ansarialiakbar <ansarialiakbar2001@gmail.com>
2024-08-21 17:29:01 +0530

    added routing

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -1,11 +1,15 @@
-import './Pokemon.css'
-function Pokemon({name, image}){
+import { Link } from 'react-router-dom';
+import './Pokemon.css';
+
+function Pokemon({name, image, id}){
     return(
         <div className='pokemon'>
+            <Link to={`/pokemon/${id}`}>
             <div className='pokemon-name'>{name}</div>
             <div>
                 <img className='pokemon-image' src={image} />
             </div>
+            </Link>
         </div>
     )
 }
